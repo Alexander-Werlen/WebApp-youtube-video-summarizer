a4c4c61b73d88e69f0db4bb971c1cb3d6000453a
Werlen Alexander <Alexwerlen@gmail.com>
2023-12-20 15:02:13 -0300

    hero design finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,17 @@ function Hero() {
     <div className='heroContent'>
         <h1 className='animate-charcter'>YouTube</h1>
         <h1 className='heroHeader'>video summarizer</h1>
+        <div class="form">
+          <input type="text" name="text" autocomplete="off" required />
+          <label for="text" class="label-name">
+            <span class="content-name">
+              Video URL
+            </span>
+          </label>
+        </div>
+        <div>
+          <a href="http://localhost:3000/" className='summarize-btn'><span>Make Summary</span></a>
+        </div>
     </div>
   )
 }
